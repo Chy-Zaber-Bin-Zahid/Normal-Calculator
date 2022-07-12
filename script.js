e9d4230527e6dffe6561ec44c82aa6c6d3e49edb
Chy-Zaber-Bin-Zahid <89964035+Chy-Zaber-Bin-Zahid@users.noreply.github.com>
2022-07-12 21:26:45 +0600

    added equal plus and minus operator

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//-----Variables-----//
 let inputSecond = document.getElementById("input2");
 inputSecond.value = "0.00";
 let inputFirst = document.getElementById("input1");
@@ -6,19 +7,50 @@ let store = "";
 let indexOfStore = 0;
 let count = 0;
 
+//-----Function-----//
+function check(sign) {
+  count += 1;
+}
+
+function operation(opa) {
+  let temp = [];
+  //(+)
+  if (store.includes("+")) {
+    temp = store.split("+");
+    inputFirst.value = store;
+    inputSecond.value = parseFloat(temp[0]) + parseFloat(temp[1]);
+    temp = [];
+    store = "";
+  }
+
+  //(-)
+  if (store.includes("-")) {
+    let temp2 = "";
+    temp2 = store.indexOf("-");
+    if (temp2 !== 0) {
+      temp = store.split("-");
+      inputFirst.value = store;
+      inputSecond.value = parseFloat(temp[0]) - parseFloat(temp[1]);
+      temp = [];
+      store = "";
+    } else {
+      temp = store.split("-");
+      inputFirst.value = store;
+      console.log(temp);
+      inputSecond.value = -1 * parseFloat(temp[1]) - parseFloat(temp[2]);
+      temp = [];
+      store = "";
+    }
+  }
+}
+
 function storeNumber(num, idx = 0) {
   if (num === ".") {
-    let check = "";
-    check += num;
-    for (let i = 0; i < check.length; i++) {
-      if (check[i] === ".") {
-        count += 1;
-      }
-    }
-    console.log(count);
+    check(".");
     if ((store !== "" && store[store.length - 1] === ".") || count > 1) {
       inputFirst.value = "Syntax ERROR";
       store = "";
+      count = 0;
     } else if (idx !== 0) {
       inputFirst.value = "";
       store += num;
@@ -40,15 +72,21 @@ function storeNumber(num, idx = 0) {
 }
 
 function buttons(btnNumber) {
-  document.getElementById(btnNumber).addEventListener("click", function () {
-    if (inputFirst.value !== "") {
-      indexOfStore = store.indexOf(store.length);
-      storeNumber(document.getElementById(btnNumber).innerText, indexOfStore);
-    } else {
-      inputSecond.value = "";
-      storeNumber(document.getElementById(btnNumber).innerText);
-    }
-  });
+  if (btnNumber !== "btn-=") {
+    document.getElementById(btnNumber).addEventListener("click", function () {
+      if (inputFirst.value !== "") {
+        indexOfStore = store.indexOf(store.length);
+        storeNumber(document.getElementById(btnNumber).innerText, indexOfStore);
+      } else {
+        inputSecond.value = "";
+        storeNumber(document.getElementById(btnNumber).innerText);
+      }
+    });
+  } else {
+    document.getElementById(btnNumber).addEventListener("click", function () {
+      operation(document.getElementById(btnNumber).innerText);
+    });
+  }
 }
 
 //button 1,2,3,4....9,0,.
@@ -64,13 +102,12 @@ buttons("btn-9");
 buttons("btn-0");
 buttons("btn-.");
 
-// document.getElementById("btn-1").addEventListener("click", function () {
-//   if (inputFirst !== "") {
-//     indexOfStore = store.indexOf(store.length);
-//     storeNumber(document.getElementById("btn-1").innerText, indexOfStore);
-//   } else {
-//     inputSecond.value = "";
-//     console.log(store);
-//     storeNumber(document.getElementById("btn-1").innerText);
-//   }
-// });
+//+,-,/,*,%
+buttons("btn-+");
+buttons("btn--");
+buttons("btn-*");
+buttons("btn-/");
+buttons("btn-%");
+
+//(=)
+buttons("btn-=");
